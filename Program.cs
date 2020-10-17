6abbf6675a1eaf7224c73aab10f1a38de88a8606
Rakesh Kanani <rkanani@horizontal.com>
2020-10-17 22:39:52 +0530

    added aoth0

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazor.Auth0;
+using Blazor.Auth0.Models.Enumerations;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Syncfusion.Blazor;
@@ -20,11 +22,21 @@ namespace YtScraper
            
             builder.RootComponents.Add<App>("app");
           
-            
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzMxNDM3QDMxMzgyZTMzMmUzMEpuSVl2TmJBa0tHZGhTaXd6Mjg4OEhXNitXYTB4RkQreGVSZHlvS3BzUUk9");
             builder.Services.AddSyncfusionBlazor();
-         
+            builder.Services.AddBlazorAuth0(options =>
+            {
+                options.Domain = "uwb.auth0.com";
+                options.ClientId = "C7I2c6OiBj9hjtJaVkWcq4KUCJZY8TsB";
+                options.RequireAuthenticatedUser = true;
+            
+                options.LoginMode = LoginModes.Redirect;
+                //options.ClientSecret = "NEVER!!";
+                //options.SlidingExpiration = true;
+            });
+            builder.Services.AddAuthorizationCore();
             await builder.Build().RunAsync();
         }
     }
